3fc03c2d7c6d270500728a891e461dab210d7366
Drew Patel <drew1patel@gmail.com>
2018-09-09 00:59:03 -0600

    Added changes

--- src/components/CollectCredentials.js
+++ src/components/CollectCredentials.js
@@ -5,8 +5,12 @@ import {uport} from '../utilities/uportSetup'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import * as AppActions from '../actions/AppActions'
-
+import * as ScanditSDK from "scandit-sdk";
 import styled from 'styled-components'
+
+import {BrowserBarcodeReader, VideoInputDevice, video} from '@zxing/library';
+
+const codeReader = new BrowserBarcodeReader();
 
 const CredentialsWrap = styled.section `
   @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
@@ -42,8 +46,11 @@ const SubText = styled.p `
 const RELATIONSHIPCLAIM = 'User'
 const CERTIFICATECLAIM = 'uPort Demo'
 
+
+
 class CollectCredentials extends Component {
 
+   
     constructor(props) {
         super(props)
         this.credentialsbtnClickA = this
@@ -57,20 +64,27 @@ class CollectCredentials extends Component {
             .bind(this)
         this.credentialsbtnClickD = this
             .credentialsbtnClickD
-            .bind(this)   
+            .bind(this)
         this.state = {
-          name: this.props.uport.name,
-          address: '',
-          party: '',
-      };
-      this.handleChange = this.handleChange.bind(this);
+            name: this.props.uport.name,
+            address: '',
+            party: ''
+        };
+        this.handleChange = this
+            .handleChange
+            .bind(this);
     }
 
-    handleChange (event) {
-      // check it out: we get the evt.target.name (which will be either "email" or "password")
-      // and use it to target the key on our `state` object with the same name, using bracket syntax
-      this.setState({ [event.target.name]: event.target.value });
-      console.log(this.state)
+    
+    
+    handleChange(event) {
+        // check it out: we get the evt.target.name (which will be either "email" or
+        // "password") and use it to target the key on our `state` object with the same
+        // name, using bracket syntax
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        console.log(this.state)
     }
 
     credentialsbtnClickA() {
@@ -111,70 +125,51 @@ class CollectCredentials extends Component {
     }
 
     credentialsbtnClickD() {
-      let voter = 
-      uport.attestCredentials({
-        sub: this.props.uport.address,
-        claim: {
-            voterRegistration: {
-              "name": this.state.name,
-              "address": this.state.address,
-              "party": this.state.party,
+        let voter = uport.attestCredentials({
+            sub: this.props.uport.address,
+            claim: {
+                voterRegistration: {
+                    "name": this.state.name,
+                    "address": this.state.address,
+                    "party": this.state.party
+                }
+            },
+            exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+            uriHandler: (log) => {
+                console.log(log)
             }
-        },
-        exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
-        uriHandler: (log) => {
-            console.log(log)
-        }
 
-    })
+        })
     }
 
     handleSubmit(event) {
-      alert('An essay was submitted: ' + this.state.value);
-      event.preventDefault();
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
     }
 
     render(props) {
+        
         return (
-            <CredentialsWrap>
+                
+                <form>
                 <h4>Register user to vote.</h4>
-                <CredentialsArea>
-                    <CredsTable>
-                        <tbody>
-                            <tr>
-                                <td
-                                    style={{
-                                    "paddingRight": "8em"
-                                }}>
-                                    <CredsLabel>Name: {this.props.uport.name}</CredsLabel>
-                                </td>
-                                <form>
-                                    <CredsLabel>
-                                        Name:
-                                        <input type="text" name="name" placeholder={this.props.uport.name} value={this.state.name} onChange={this.handleChange} disabled/>
-                                    </CredsLabel>
-                                    <CredsLabel>
-                                        Address:
-                                        <input type="text" name="address" value={this.state.address} onChange={this.handleChange}/>
-                                    </CredsLabel>
-                                    <CredsLabel>
-                                        Party:
-                                        <input type="text" name="party" value={this.state.party} onChange={this.handleChange}/>
-                                    </CredsLabel>
-                                    
-                                 
-                                </form>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <CredsButton onClick={this.credentialsbtnClickD}>Set</CredsButton>
-                        </tbody>
-                    </CredsTable>
-                    <NextButton onClick={this.props.actions.credentialsDemoComplete}>Next</NextButton>
-                </CredentialsArea>
-                <SubText>Credentials take a moment to appear on your device.</SubText>
-            </CredentialsWrap>
+                    <input
+                        id="name"
+                        placeholder='Name'
+                        value={this.props.uport.name}
+                        onChange={this.handleChange}/>
+                    <input
+                        id="address"
+                        placeholder='Address'
+                        value={this.props.uport.address}
+                        onChange={this.handleChange}/>
+                    <input
+                        id="party"
+                        placeholder='Party'
+                        value={this.props.uport.party}
+                        onChange={this.handleChange}/>
+                    <CredsButton onClick={this.credentialsbtnClickD}>Set</CredsButton>
+                </form>
         )
     }
 }
